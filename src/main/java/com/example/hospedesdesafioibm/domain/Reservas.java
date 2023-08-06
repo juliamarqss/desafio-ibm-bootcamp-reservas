98464381ac5accd78cf157cdaf76fa37314b98ee
Julia <juliamarques61@gmail.com>
2023-08-06 16:37:11 -0300

    refactor: retirando as annotations e acrescentando os constructos, getters e setters em Reservas

--- src/main/java/com/example/hospedesdesafioibm/domain/Reservas.java
+++ src/main/java/com/example/hospedesdesafioibm/domain/Reservas.java
@@ -1,28 +1,22 @@
 package com.example.hospedesdesafioibm.domain;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import java.io.Serializable;
 import java.sql.Date;
 
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
 public class Reservas implements Serializable {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String nomeHospede;
 
-//    @JsonFormat(pattern = "dd/MM/yyyy")
+    //    @JsonFormat(pattern = "dd/MM/yyyy")
     private Date dataInicio;
 
     private Date dataFim;
@@ -30,4 +24,64 @@ public class Reservas implements Serializable {
     private Integer quantidadePessoas;
 
     private Boolean status; // Precisa retornar como "CONFIRMADA" "PENDENTE"
+
+    public Reservas() {
+    }
+
+    public Reservas(Integer id, String nomeHospede, Date dataInicio, Date dataFim, Integer quantidadePessoas, Boolean status) {
+        this.id = id;
+        this.nomeHospede = nomeHospede;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.quantidadePessoas = quantidadePessoas;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomeHospede() {
+        return nomeHospede;
+    }
+
+    public void setNomeHospede(String nomeHospede) {
+        this.nomeHospede = nomeHospede;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public Integer getQuantidadePessoas() {
+        return quantidadePessoas;
+    }
+
+    public void setQuantidadePessoas(Integer quantidadePessoas) {
+        this.quantidadePessoas = quantidadePessoas;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
 }
