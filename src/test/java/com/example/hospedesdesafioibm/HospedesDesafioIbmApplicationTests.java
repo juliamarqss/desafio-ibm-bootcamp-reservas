3b11e358d0215be09ce8592ccd96ad3f621c6b45
Julia <juliamarques61@gmail.com>
2023-08-06 15:42:55 -0300

    feat: Iniciando o projeto

---
+++ src/test/java/com/example/hospedesdesafioibm/HospedesDesafioIbmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospedesdesafioibm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospedesDesafioIbmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
