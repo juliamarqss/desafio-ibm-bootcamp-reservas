3c83e4b74db55a74d95d5fbc26e7c558a80edf13
Julia <juliamarques61@gmail.com>
2023-08-08 16:46:58 -0300

    feat: model reserve criada

---
+++ frontend/src/app/components/reserve/models/reserve.model.ts
@@ -0,0 +1,14 @@
+export interface Reserves {
+  id?: number,
+  nomeHospede: string,
+  dataInicio: string,
+  dataFim: string,
+  quantidadePessoas: number,
+  status?: string
+}
+
+// export enum StatusReserva {
+//   "CONFIRMADA",
+//   "PENDENTE",
+//   "CANCELADA"
+// }
